b6b6b2c82fb1c1c97f5ed87917f59a4362e310f6
Prajjwal <prajjwal5853@gmail.com>
2024-03-03 18:58:07 +0545

    Changes in false position

--- FalsePosition.c
+++ FalsePosition.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <math.h>
-#define errorTolerance 0.001
+#define epsilon 0.001
 
 float f(float x)
 {
@@ -8,8 +8,8 @@ float f(float x)
     //return sin(x) - x + 2;
     //return pow(x,3)-4*pow(x,2)+x+6;
     //return x*sin(x) + cos(x);
-    // return pow(x,3) + 4*x - 1;
-    return pow(x,6) - pow(x,4) - pow(x,3) - 1;
+    return pow(x,3) + 4*x - 1;
+    //return pow(x,6) - pow(x,4) - pow(x,3) - 1;
 }
 float func(float a, float b)
 {
@@ -18,17 +18,13 @@ float func(float a, float b)
 int main()
 {
     int iteration = 0;
-    float product, x, a, b, error, prev;
+    float product, x, a, b, error;
     do
     {
         printf("Enter the initial guess a and b:");
         scanf("%f%f", &a, &b);
-        product = f(a) * f(b);
-        if(product<0)
-        {
-            break;
-        }
-        else if (product > 0)
+        product = f(a) * f(b);        
+        if (product > 0)
         {
             printf("\nRe enter the initial values:");
         }
@@ -37,20 +33,20 @@ int main()
             printf("the root is %lf", (f(a) == 0) ? a : b);
             return 0;
         }
-    } while (1);
+    } while (product>0);
 
-    printf("Given equation is ...\n");
-    printf("f(%d)=%lf\nf(%d)=%lf\n",(int)a,f(a),(int)b,f(b));
-    printf("Since, f(%d) x f(%d) <0.So, root lies between %d and %d\n\n",(int)a,(int)b,(int)a,(int)b);
+    //printf("Given equation is ...\n");
+    //printf("f(%d)=%lf\nf(%d)=%lf\n",(int)a,f(a),(int)b,f(b));
+    //printf("Since, f(%d) x f(%d) <0.So, root lies between %d and %d\n\n",(int)a,(int)b,(int)a,(int)b);
     printf("S.N\ta\t\tb\t\tf(a)\t\tf(b)\t\tx\t\tf(x)\n");
     
     do
     {
         iteration++;
-        if (iteration > 1)
-        {
-            prev = x;
-        }
+        // if (iteration > 1)
+        // {
+        //     prev = x;
+        // }
         x = func(a, b);
         printf("%d\t%.4lf\t\t%.4lf\t\t%.4lf\t\t%.4lf\t\t%.4lf\t\t%.4lf\n",iteration,a,b,f(a),f(b),x,f(x));
         if ((f(a) * f(x)) > 0)
@@ -61,15 +57,12 @@ int main()
         {
             b = x;
         }
-
-        
-        error = fabs(x - prev);
-        if(iteration>1 && error<errorTolerance)
-        {
+        error = fabs(b-a);
+        if(fabs(f(x))<epsilon || error<epsilon || iteration==15){
             break;
-        }
-        
+        }      
     } while (1);
-    printf("Since,aproximate error<error tolerance i.e %.4lf<%.3lf, the root is %.3lf",error,errorTolerance,x);
+    //printf("Since,aproximate error<error tolerance i.e %.4lf<%.3lf, the root is %.3lf",error,epsilon,x);
+    printf("The root is %.3lf",x);
    
 }
